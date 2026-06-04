072325baf6ab8d3f86d35e097476e7268cc653fd
JStrong <jstrong1105@naver.com>
2026-06-04 17:47:34 +0900

    2026-06-04 17:47 Party.java / Theme.java Controller 메소드 / URL 패턴 / 유효성 검사 목록 정의

--- src/main/java/com/noexit/app/controller/Theme.java
+++ src/main/java/com/noexit/app/controller/Theme.java
@@ -18,11 +18,23 @@ public class Theme
     Theme(CafeController cafeController) {
         this.cafeController = cafeController;
     }
+    
+    /*
+     * themelist 페이지로 이동하는 메소드
+     * 파라미터로 schTpye, kwd 를 받고 
+     * 키워드가 존재한다면 바인딩해서 넘겨준다
+     */
 	@GetMapping("list")
-	public String themeList(@RequestParam(name="schType", defaultValue = "cafeName") String schType
+	public String themeListPage(@RequestParam(name="schType", defaultValue = "cafeName") String schType
 						  , @RequestParam(name="kwd", defaultValue = "") String kwd
 						  , Model model)
 	{
+		/*
+		 * 유효성 검사 목록
+		 * 
+		 * 없음
+		 */
+		
 		if(!kwd.isBlank())
 		{
 			model.addAttribute("schType", schType);
@@ -32,9 +44,45 @@ public class Theme
 		return "theme/themelist";
 	}
 	
+	/**
+	 * AJAX 요청을 받아서 데이터를 넘겨주는 메소드
+	 * 
+	 * @param schType : 검색 타입
+	 * @param kwd     : 검색 키워드
+	 * @param lastId  : 마지막으로 받은 데이터 번호
+	 * @param model  
+	 * @return
+	 */
+	@PostMapping()
+	public String themeListItem(@RequestParam(name="schType", defaultValue = "cafeName") String schType
+						  , @RequestParam(name="kwd", defaultValue = "") String kwd
+						  , @RequestParam(name="lastId", defaultValue = "0") long lastId
+						  , Model model)
+	{
+		/*
+		 * 유효성 검사 목록
+		 * 
+		 * 없음
+		 */
+		
+		return "";
+	}
+	
+	/**
+	 * 특정 테마의 세부 정보를 찾아주는 메소드
+	 * 
+	 * @param themeId : 테마 번호
+	 * @param model  
+	 * @return
+	 */
 	@GetMapping("info/{themeid}")
 	public String themeDetail(@PathVariable(name="themeid") int themeId, Model model)
 	{
+		/*
+		 * 유효성 검사 목록
+		 * 
+		 * 존재하는 themeId 인가?
+		 */
 		
 		return "theme/themeinfo";
 	}
@@ -45,12 +93,4 @@ public class Theme
 		return "theme/themeEnrollForm";
 	}
 	
-	@PostMapping()
-	public String themeList(@RequestParam(name="schType", defaultValue = "cafeName") String schType
-						  , @RequestParam(name="kwd", defaultValue = "") String kwd
-						  , @RequestParam(name="lastId", defaultValue = "0") long lastId
-						  , Model model)
-	{
-		return "";
-	}
 }
